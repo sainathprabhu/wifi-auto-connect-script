dbea6ea1214268c31c4ef9c70ec3a754a172a0ab
sainath prabhu <sainathprabhu93@gmail.com>
2019-04-03 10:03:12 +0530

    Update connect.sh

--- connect.sh
+++ connect.sh
@@ -1,2 +1,2 @@
-sudo networksetup setairportnetwork en0 Bloomreach S@mtechn0
---sudo networksetup setairportnetwork en0 'SSID' 'S@mtechn0'
+sudo networksetup setairportnetwork en0 
+--sudo networksetup setairportnetwork en0 'SSID' 'password'
